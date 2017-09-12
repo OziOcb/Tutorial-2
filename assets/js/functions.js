132b796a448649e1abaf86fe6aac6d86bd6523fd
OziOcb <31310132+OziOcb@users.noreply.github.com>
2017-09-12 19:46:20 +0100

    after hour 7.3

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,16 +7,24 @@ function mentoringBubbleClick() {
     var $this = $(this),
         faceTop = $this.position().top,
         vertMath = -1 * (faceTop - 230);
+        faceLeft = $this.position().left,
+        horizMath = 0 - faceLeft;
 
-    $this.parent().css('top', + vertMath +'px');
-
-    $this.addClass('has-bubble-open')
-      .siblings().removeClass('has-bubble-open');
-
+    if($(window).width() > 640){
+      $this.parent().css('top', + vertMath +'px');
+    } else {
+      if($this.hasClass('back-btn')){
+        mentoringNarrowStart();
+      } else {
+        $this.parent().css('left', + horizMath +'px');
+      }
+    }
+    if(!$this.hasClass('back-btn')){
+      $this.addClass('has-bubble-open')
+        .siblings().removeClass('has-bubble-open');
+    }
   });
-
 }
-
 
 
 $(window).scroll(function(){
@@ -30,18 +38,51 @@ function youtubeVidScroll() {
   var wScroll = $(window).scrollTop();
 
   $('.video-strip').css('background-position','center -'+ wScroll +'px')
-
 }
-
 
 function startMentoring() {
 
   var wScroll = $(window).scrollTop();
 
-  if($('section.mentoring').offset().top - 500 < wScroll ) {
-    $('.faces').addClass('launched');
-    setTimeout(function(){
-      $('.face:nth-child(3)').addClass('has-bubble-open');
-    }, 400);
+  if($('section.mentoring').offset().top - 500 < wScroll) {
+    if($(window).width() > 640) {
+      $('.faces').addClass('launched');
+
+      if(!$('.face').hasClass('has-bubble-open') && !$this.hasClass('back-btn')){
+        setTimeout(function(){
+          $('.face:nth-child(3)').addClass('has-bubble-open');
+        }, 400);
+      }
+    } else {
+      mentoringNarrowStart();
+    }
   }
 };
+
+
+
+function mentoringNarrowStart() {
+  $('.faces').css({
+    'top': '230px',
+    'left': '0px'
+  });
+  $('.face').first().addClass('has-bubble-open')
+    .siblings().removeClass('has-bubble-open');
+}
+
+function mentoringWideStart() {
+  $('.faces').css({
+    'top': '0px',
+    'left': '0px'
+  });
+  $('.face:nth-child(3)').addClass('has-bubble-open')
+    .siblings().removeClass('has-bubble-open');
+}
+
+$(window).resize(function() {
+  if($(window).width() > 640){
+    mentoringWideStart();
+  } else {
+    mentoringNarrowStart();
+  }
+});
